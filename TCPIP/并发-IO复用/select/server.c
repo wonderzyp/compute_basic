d0ee70089a5c370149f9f88520afc3b9bfad7742
zyp-wsl <2671078523@qq.com>
2022-05-09 18:23:17 +0800

    select实现IO复用

---
+++ TCPIP/并发-IO复用/select/server.c
@@ -0,0 +1,88 @@
+/**
+* @brief 基于select实现I/O复用并发服务端
+* ****************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+#define BUF_SIZE 100
+void error_handling(char *buf);
+
+int main(int argc, char *argv[]){
+  int serv_sock, clnt_sock;
+  struct sockaddr_in serv_adr, clnt_adr;
+  struct timeval timeout;
+  fd_set reads, cpy_reads;
+
+  socklen_t adr_sz;
+  int fd_max, str_len, fd_num, i;
+  char buf[BUF_SIZE];
+  if (argc!=2){
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  memset(&serv_adr, 0, sizeof(serv_adr));
+  serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_port = htons(atoi(argv[1]));
+
+  if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+    error_handling("bind() error\n");
+  if (listen(serv_sock, 5)==-1)
+    error_handling("listen() error\n");
+
+  FD_ZERO(&reads);
+  FD_SET(serv_sock, &reads); // 注册监视对象serv_sock
+  fd_max = serv_sock;
+
+  while (1)
+  {
+    cpy_reads = reads;  // 由于监视事件发生后，对应reads会被置0(除被监视位外)
+                        // 拷贝副本以还原为初始态
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 5000;
+
+    if ((fd_num = select(fd_max+1, &cpy_reads, 0,0,&timeout))==-1) 
+      break;
+    if (fd_num==0)
+      continue;
+    
+    for(int i=0; i<fd_max+1; ++i){ // 遍历所有监视集合，查询是否有事件发生
+      if (FD_ISSET(i, &cpy_reads)){
+        if (i==serv_sock){ // 连接请求
+          adr_sz = sizeof(clnt_adr);
+          clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+          FD_SET(clnt_sock, &reads);   // 将连接套接字注册
+          if (fd_max < clnt_sock)
+            fd_max = clnt_sock;
+          printf("connected client: %d\n", clnt_sock);
+        }else{  // 注册的连接套接字发生更新
+          str_len = read(i, buf, BUF_SIZE);
+          if (str_len == 0){ // 读取完毕，关闭
+            FD_CLR(i, &reads);
+            close(i);
+            printf("closed client: %d\n",i);
+          }else{
+            write(i,buf,str_len);
+          }
+        }
+      }
+    }
+  }
+  close(serv_sock);
+  return 0;
+}
+
+void error_handling(char *message){
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
+
